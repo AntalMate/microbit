8e8d5d478d88fcdb5ddd0fc3ab913aa9a99db183
AntalMate <113516211+AntalMate@users.noreply.github.com>
2022-11-10 09:02:21 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,67 @@
+let index = 0
+input.onButtonPressed(Button.A, function () {
+    if (index < 5) {
+        index += 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (index > 0) {
+        index += -1
+    }
+})
+basic.forever(function () {
+    if (index == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (index == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    if (index == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            # # . . .
+            `)
+    }
+    if (index == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # . .
+            # # # . .
+            `)
+    }
+    if (index == 4) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # # .
+            . # # # .
+            # # # # .
+            `)
+    }
+    if (index == 5) {
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . # # #
+            . # # # #
+            # # # # #
+            `)
+    }
+})
